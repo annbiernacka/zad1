b2a7354b5afe58a473d29f1768c74afc9fdb9127
annbiernacka <ania2915@op.pl>
2018-02-05 01:14:31 +0100

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PokemonFormComponent } from './pokemon-form/pokemon-form/pokemon-form.component';
+import { TextComponent } from './text/text.component';
 
+const appRoutes: Routes = [
+  { path: 'form', component: PokemonFormComponent },
+  { path: 'text', component: TextComponent }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonFormComponent,
+    TextComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
     BrowserModule,
     FormsModule,
     HttpModule
